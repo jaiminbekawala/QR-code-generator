98958dc036768debdcc56819f5a502f045c4ffac
Jaimin Bekawala <64148623+jaiminbekawala@users.noreply.github.com>
2024-07-03 01:09:16 +0200

    Second Commit
    Adding all the necessary files

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded',() => {
+    const form = document.getElementById('qr-form');
+    const qrCanvas = document.getElementById('qr-code');
+
+    form.addEventListener('submit', handleasyncClick) => {
+        Event.preventDefault();
+        const menu_Url = document
+    }
+}
+)
